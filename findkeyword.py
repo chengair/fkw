14929beee012839ae5ea191f4c728eb4e9a13122
cgrgmcc <chengairun@139.com>
2020-01-14 17:33:06 +0800

    3

--- findkeyword.py
+++ findkeyword.py
@@ -1,5 +1,14 @@
 import jieba
 import os
+import sqlite3
+from collections import defaultdict
+
+class FindKeyWords():
+    def __init__(self):
+        super().__init__()
+        print(1)
 
 
-print(1)
+if __name__ == "__main__":
+    fkw=FindKeyWords()
+    pass
